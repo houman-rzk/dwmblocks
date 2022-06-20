50aa4ab798ba4b28d180d1dc2cc1d50979c4528a
ache <ache@ache.com>
2022-06-20 21:24:03 +0200

    Minor changes

--- blocks.h
+++ blocks.h
@@ -1,12 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "sb-mem",								10,		2},
+	{"", "sb-mem",								10,		1},
 	{"", "sb-forecast",							7200,		2},
-	{"", "sb-clock",							30,		2},
-	{"", "sb-network",							10,		2},
+	{"", "sb-tasks",							0,		5},
+	{"", "sb-clock",							30,		3},
+	{"", "sb-network",							10,		4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char *delim = " | ";
+static char *delim = " ┃ ";
+//static char *delim = " • ";
 //static unsigned int delimLen = 5;
