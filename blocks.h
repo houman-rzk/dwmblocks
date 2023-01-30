18bdcbfda4aa75b987450b31dd20719f75c116a3
hache <null>
2023-01-30 21:29:46 +0100

    Changed some update intervals. Added a block

--- blocks.h
+++ blocks.h
@@ -1,12 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	//{"", "sb-timer",							0,		9},
 	{"", "sb-mem",								30,		1},
-	{"", "sb-forecast",							7200,	2},
+	{"", "sb-forecast",							120,    2},
 	{"", "sb-tasks",							0,		5},
-	{"", "sb-music",							60,		7},
+	{"", "sb-music",							0,		7},
 	{"", "sb-volume",							60,		6},
-	{"", "sb-battery",							60,     8},
+	{"", "sb-battery",							120,    8},
 	{"", "sb-clock",							30,		3},
 	{"", "sb-network",							30,		4},
 };
