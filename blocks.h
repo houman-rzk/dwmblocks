5fbd4f0f90ce5715b179b71021fd719d0abc0dc1
Houman <houman@houmanr.xyz>
2023-10-17 11:00:59 +0200

    Changed a few intervals and signals

--- blocks.h
+++ blocks.h
@@ -2,14 +2,14 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	//{"", "sb-timer",							0,					9},
-	{"", "sb-mem",									30,					1},
-	{"", "sb-forecast",							120,				2},
-	{"", "sb-tasks",								0,					5},
+	{"", "sb-mem",									30,					5},
+	{"", "sb-forecast",							60, 				2},
+	{"", "sb-tasks",								0,					1},
 	{"", "sb-music",								0,					7},
 	{"", "sb-volume",								60,					6},
 	{"", "sb-battery",							120,  			8},
 	{"", "sb-clock",								30,					3},
-	{"", "sb-network",							10,					4},
+	{"", "sb-network",							30,					4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
